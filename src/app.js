10bd2026fa6b8807fad6ffa862863f174c6cec7c
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-21 04:39:35 -0300

    [Updated] Setup for heroku

--- src/app.js
+++ src/app.js
@@ -1,59 +1,60 @@
 // @node_modules
-const express = require('express');
-const hbs = require('hbs');
-const path = require('path');
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
 
 // @own_modules
-const geoCode = require('./utils/geocode');
-const forecast = require('./utils/forecast');
-const geocode = require('./utils/geocode');
-const { send } = require('process');
-const { request } = require('express');
+const geoCode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
+const { send } = require("process");
+const { request } = require("express");
 
 // @app_constants
 const app = express();
+const port = process.env.PORT || 3000;
 
 // @Define paths for Express config
-const partialsPath = path.join(__dirname, '../templates/partials');
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
 
 // @Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
 // @Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-  res.render('index', {
-    title: ' Weather - App ',
-    name: 'Erick Vicentin',
+app.get("", (req, res) => {
+  res.render("index", {
+    title: " Weather - App ",
+    name: "Erick Vicentin",
   });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About Me',
-    name: 'Erick Vicentin',
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Erick Vicentin",
   });
 });
 
-app.get('/help', (req, res) => {
-  res.render('help', {
-    title: 'Help',
-    name: 'Erick Vicentin',
-    helpText: 'This is some helpful text.',
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Erick Vicentin",
+    helpText: "This is some helpful text.",
   });
 });
 
-app.get('/weather', (req, res) => {
+app.get("/weather", (req, res) => {
   const address = req.query.address;
 
-  if (!address || address === '') {
+  if (!address || address === "") {
     res.send({
-      error: 'Input an valid location',
+      error: "Input an valid location",
     });
   } else {
     geocode(address, (geoError, { latitude, longitude, location }) => {
@@ -76,13 +77,13 @@ app.get('/weather', (req, res) => {
   }
 });
 
-app.get('*', (req, res) => {
-  res.render('missing', {
-    title: '404 Not Found!',
-    name: 'Erick Vicentin',
+app.get("*", (req, res) => {
+  res.render("missing", {
+    title: "404 Not Found!",
+    name: "Erick Vicentin",
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+app.listen(port, () => {
+  console.log(`Server is up on port ${PORT}.`);
 });
